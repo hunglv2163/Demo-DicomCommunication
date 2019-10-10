3b162812dcf212d0fae11b99d9d8bd1165207849
cuongItleadPro <khuccuong79@gmail.com>
2019-10-10 10:57:24 +0700

    storeSCP

---
+++ dcm4che-tool-dcmecho/src/main/java/org/dcm4che2/tool/dcmecho/Test.java
@@ -0,0 +1,9 @@
+package org.dcm4che2.tool.dcmecho;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] para = new String[1];
+        para[0] = "STORESCP@localhost:4444";
+        DcmEcho.main(para);
+    }
+}
