bd71d1f2eb2341eb6e49cfd7a531f39c006a9d90
cuongItleadPro <khuccuong79@gmail.com>
2019-10-11 11:43:45 +0700

    "update"

--- dcm4che-tool-dcmhpqr/src/main/java/org/dcm4che2/tool/dcmhpqr/Test.java
+++ dcm4che-tool-dcmhpqr/src/main/java/org/dcm4che2/tool/dcmhpqr/Test.java
@@ -1,8 +1,12 @@
 package org.dcm4che2.tool.dcmhpqr;
 
+import org.dcm4che2.tool.dcmqr.DcmQR;
+
 public class Test {
     public static void main(String[] args) {
-
-        DcmHPQR.main();
+        String[] para = new String[2];
+        para[0] = "STORESCP@localhost:4444";
+        para[1] = "qStudyDate=20060204";
+        DcmQR.main(para);
     }
 }
